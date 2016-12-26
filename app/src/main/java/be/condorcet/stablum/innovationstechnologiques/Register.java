74973fc5401520914a1bb0370ec88d3a9b151ebe
N1portekoi <sludovic@skynet.be>
2016-12-26 23:43:37 +0100

    Interface Register

---
+++ app/src/main/java/be/condorcet/stablum/innovationstechnologiques/Register.java
@@ -0,0 +1,17 @@
+package be.condorcet.stablum.innovationstechnologiques;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Ludovic on 26-12-16.
+ */
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}

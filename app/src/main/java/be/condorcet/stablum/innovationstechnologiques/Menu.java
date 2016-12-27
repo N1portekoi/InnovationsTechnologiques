486ea3c33d505ecd75414f9a9f1c11fc4782c903
N1portekoi <sludovic@skynet.be>
2016-12-27 01:50:23 +0100

    Interfaces

--- app/src/main/java/be/condorcet/stablum/innovationstechnologiques/Menu.java
+++ app/src/main/java/be/condorcet/stablum/innovationstechnologiques/Menu.java
@@ -1,7 +1,9 @@
 package be.condorcet.stablum.innovationstechnologiques;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Ludovic on 27-12-16.
@@ -12,5 +14,29 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+    }
+
+    protected void onClickScore(View view) {
+        Intent intent = new Intent(Menu.this, Score.class);
+        startActivity(intent);
+    }
+
+    protected void onClickTop10(View view) {
+        Intent intent = new Intent(Menu.this, Top10.class);
+        startActivity(intent);
+    }
+
+    protected void onClickGames (View view) {
+        Intent intent = new Intent(Menu.this, Games.class);
+        startActivity(intent);
+    }
+
+    protected void onClickUsers (View view) {
+        Intent intent = new Intent(Menu.this, Users.class);
+        startActivity(intent);
+    }
+
+    protected void onClickSignOut (View view) {
+
     }
 }
---
+++ app/src/main/java/be/condorcet/stablum/innovationstechnologiques/Top10.java
@@ -0,0 +1,16 @@
+package be.condorcet.stablum.innovationstechnologiques;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ludovic on 27-12-16.
+ */
+
+public class Top10 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.top10);
+    }
+}
